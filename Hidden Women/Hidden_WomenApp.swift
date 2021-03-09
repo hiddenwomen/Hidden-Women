0ee611dcd7af114015d9e1b939993266e92a028a
Claudia Marzal <claudiamarzalcri@gmail.com>
2021-03-09 08:54:06 +0100

    Initial Commit

---
+++ Hidden Women/Hidden_WomenApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hidden_WomenApp.swift
+//  Hidden Women
+//
+//  Created by Claudia Marzal Polop on 9/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Hidden_WomenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
